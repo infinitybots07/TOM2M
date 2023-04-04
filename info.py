9951968ea26a77a8ab5420982f060bd0936d2313
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-04-04 14:22:59 +0530

    Update info.py

--- info.py
+++ info.py
@@ -121,8 +121,8 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 LANGUAGES = ["malayalam", "tamil", "english", "hindi", "telugu", "kannada"]
 
 # Delete Time
-DELETE_TIME = int(environ.get('DELETE_TIME', 300))
-IMDB_DLT_TIME = int(environ.get('IMDB_DLT_TIME', 300))
+DELETE_TIME = int(environ.get('DELETE_TIME', 600))
+MANUAL_TIME = int(environ.get('MANUAL_TIME', 20))
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
