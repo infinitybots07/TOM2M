772d15efe7026379df2f197f6cb44d56c100d9f8
Infinity Botz <134354677+infinitybots07@users.noreply.github.com>
2023-06-04 20:12:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/infinitybots07/ctom2m.git /ctom2m
+  git clone https://github.com/infinitybots07/TOM2M.git /TOM2M
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /ctom2m
+  git clone $UPSTREAM_REPO /Tom2M
 fi
-cd /ctom2m
+cd /Tom2M
 pip3 install -U -r requirements.txt
 echo "Starting...."
 python3 bot.py
